55bbf60a8925266a1a33d0f2758cbb633d4ecc89
Parvis Nguyen <parvis_nguyen@hotmail.com>
2025-03-03 20:02:31 -0500

    Added searching artists

--- api_test.py
+++ api_test.py
@@ -14,44 +14,50 @@ sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(
     client_secret=os.getenv("SPOTIPY_CLIENT_SECRET")
 ))
 
+while True:
 # Searches for an artist in a query
 # q is the search query
 # type is the type of item to search for
-search_query = "Porter Robinson"
-result = sp.search(q=search_query, type="artist")
+    search_query = input("Enter an artist or type 'exit' to exit: ").strip()
+    result = sp.search(q=search_query, type="artist")
+    
+    # Exit the program if the user types 'exit'
+    if search_query.lower () == 'exit':
+        break
 
 #Checks if there is an artist found
-if result['artists']['items']:
+    if result['artists']['items']:
     
 # Extract artist info
 # result is a dictionary returned by sp.search()
 # 'artist' contains artists found in the search query
 # 'items' contains the list of artists found
 # [0] is used to get the first artist in the list
-    artist = result['artists']['items'][0]
-    artist_id = artist['id']
-    artist_name = artist['name']    
-    followers = artist['followers']['total']
-    genres = artist['genres']
-    popularity = artist['popularity']
+        artist = result['artists']['items'][0]
+        artist_id = artist['id']
+        artist_name = artist['name']    
+        followers = artist['followers']['total']
+        genres = artist['genres']
+        popularity = artist['popularity']
 
 # Extract and display artist info
-    print(f"\n🎵 Artist: {artist_name}")
-    print(f"👥 Followers: {followers}")
+        print(f"\n🎵 Artist: {artist_name}")
+        print(f"👥 Followers: {followers}")
 # Checks if genres is not empty, if it is then it will print 'No genres available'
-    print(f"🎼 Genre(s): {', '.join(genres) if genres else 'No genres available'}")
-    print(f"🔥 Popularity: {popularity}/100")
+        print(f"🎼 Genre(s): {', '.join(genres) if genres else 'No genres available'}")
+        print(f"🔥 Popularity: {popularity}/100")
 
 # Takes top tracks of artist
-    top_tracks = sp.artist_top_tracks(artist_id)['tracks']
+        top_tracks = sp.artist_top_tracks(artist_id)['tracks']
 
 # Extract track details
-    track_data=[]
-    for track in top_tracks:
-        track_data.append([track['name'], track['album']['name'], track['popularity']])
+        track_data=[]
+        for track in top_tracks:
+            track_data.append([track['name'], track['album']['name'], track['popularity']])
     
-    print("\n🎵 Top Tracks:")
-    print(tabulate(track_data, headers=['Track', 'Album', 'Popularity'], tablefmt='pretty'))
+        print("\n🎵 Top Tracks:")
+        print(tabulate(track_data, headers=['Track', 'Album', 'Popularity'], tablefmt='pretty'))
 
-else:
-    print (f"No Aritst found for the query: '{serach_query}'.")
+    else:
+        print (f"No Aritst found for the query: '{search_query}'.")
+        
